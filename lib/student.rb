d41d2f642fa2fa358c14410106552799adf2fadb
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 22:40:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
     @@all << self
   end
 
-  def self.create_table()
+  def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
@@ -23,14 +23,21 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students 
+      INSERT INTO students
       (name, grade)
       VALUES (?, ?) ;
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    # Grab the id 
+    # Grab the id
     DB{:conn}.execute("SELECT last_insert_rowid() FROM students;")[0][0]
   end
 
