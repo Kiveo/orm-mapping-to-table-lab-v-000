8c1f1f253e7a307b31f6080095f9523d9bd38955
Maverick Tesini <mavericktesini@gmail.com>
2018-06-28 22:44:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ class Student
       VALUES (?, ?) ;
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    # Grab the id
-    DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
+    # Grab and assign the id
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
   end
 
   def self.create(name:, grade:)
